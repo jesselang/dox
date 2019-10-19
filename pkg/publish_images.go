380c56a30736cdb38592dd4ebddefa0d60df9b19
Jesse Lang <jesse@jesselang.com>
2019-10-19 11:02:10 -0500

    Comment out code that go vet says is unreachable

--- pkg/publish_images.go
+++ pkg/publish_images.go
@@ -38,7 +38,8 @@ func getImageSrcsFromHTML(content string) ([]string, error) {
 			}
 		}
 	}
-	return imageSrcs, nil
+	// Commented out because go vet complains that this in unreachable.
+	// return imageSrcs, nil
 }
 
 func getImageSrcFiles(content string, file string) ([]string, error) {
